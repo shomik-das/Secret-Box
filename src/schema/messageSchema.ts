e17b5c1d78f4d3fe3750e58e766b3a82e20e9818
shomik-das <dashomik@gmail.com>
2025-09-10 21:35:35 +0530

    zod schema fix

--- src/schema/messageSchema.ts
+++ src/schema/messageSchema.ts
@@ -3,5 +3,5 @@ import {z} from 'zod';
 export const messageSchema = z.object({
     message: z.string()
             .min(10, {message: "Message must be at least 10 characters long"})
-            .max(200, {message: "Message must be at most 200 characters long"}
+            .max(200, {message: "Message must be at most 200 characters long"})
 });
