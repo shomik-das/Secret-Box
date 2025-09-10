af3982d3d917da0a455190d7a42e6bf7498e5fc7
shomik-das <dashomik@gmail.com>
2025-09-10 21:34:10 +0530

    zod schema define

---
+++ src/schema/messageSchema.ts
@@ -0,0 +1,7 @@
+import {z} from 'zod';
+
+export const messageSchema = z.object({
+    message: z.string()
+            .min(10, {message: "Message must be at least 10 characters long"})
+            .max(200, {message: "Message must be at most 200 characters long"}
+});
--- src/schema/signUpSchema.ts
+++ src/schema/signUpSchema.ts
@@ -1,6 +1,6 @@
 import {z} from 'zod';
 
-export const signInSchema = z.object({
+export const signUpSchema = z.object({
     name: z.string()
         .min(3, {message: "Name must be at least 3 characters long"})
         .max(20, {message: "Name must be at most 20 characters long"})
@@ -8,5 +8,3 @@ export const signInSchema = z.object({
     email: z.string().email({message: "Invalid email address"}),
     password: z.string().min(3, {message: "Password must be at least 3 characters long"}),
 });
-
-export type SignInInput = z.infer<typeof signInSchema>;
